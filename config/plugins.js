cab8f46c3893bd34f737a2a766d7c61439369f1f
Your Name <>
2022-12-19 13:17:04 +0100

    setting meilisearch

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,38 @@
 module.exports = {
-  // ...
+  meilisearch: {
+    config: {
+      article: {
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            categoryTitles: entry.categories.map(item => item.title),
+          }
+        },
+        settings: {
+          filterableAttributes: ['categoryTitles'],
+          searchableAttributes: [
+            'title',
+            'categoryTitles',
+            'slug'
+          ],
+        },
+      },
+      category: {
+        settings: {
+          searchableAttributes: ['title'],
+        },
+      },
+      faq: {
+        searchableAttributes: ['question', 'answer']
+      },
+    }
+  },
+  graphql: {
+    enabled: true,
+    config: {
+      defaultLimit: 50,
+      depthLimit: 50,
+      maxLimit: 20,
+    }
+  }
 }
