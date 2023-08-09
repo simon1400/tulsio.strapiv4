269bd6ac18c585132fdd5f5679ad9a99abf4d608
Your Name <>
2023-08-09 16:52:27 +0200

    fixed models

---
+++ src/api/product-label/routes/product-label.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-label router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-label.product-label');
---
+++ src/api/product-label/services/product-label.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-label service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-label.product-label');
